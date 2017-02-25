037926614f78302f8f1f1f2e9e1e8d740b656bf2
Jens Trio <hello@betaweb.be>
2017-02-25 20:19:03 +0100

    Group the history by day

--- src/history.js
+++ src/history.js
@@ -11,29 +11,58 @@ module.exports = (pluginContext) => {
             // load the data from the json file
             timetrack.loadData();
 
-            // render the history items
-            resolve(timetrack.getHistory().slice(0, historyLimit).map(entry => {
+            // group the items by day
+            const historyItems = timetrack.getHistory().reduce((carry, entry) => {
+                // get the timestamp at te beginning of the day
+                const dayStart = Sugar.Date.format(Sugar.Date.create(entry.start), '%F');
+                // alert(dayStart);
 
-                // get the duration of the entry
-                let duration = 'Running';
-                if ('stop' in entry) {
-                    // sugarjs sets it auto on 1 hour, so i'll just subtract 3600000 for now, fix this later
-                    duration = Sugar.Date.format(Sugar.Date.create(entry.stop - entry.start - 3600000), '%H:%M:%S');
+                // group the time registrations by day
+                if (!carry[dayStart]) {
+                    carry[dayStart] = [];
+                }
+                carry[dayStart].push(entry);
+
+                return carry;
+            }, {});
+
+            // calculate the result
+            let result = [];
+
+            // generate the result for each day
+            for (let day in historyItems) {
+                if (!historyItems.hasOwnProperty(day)) {
+                    continue;
                 }
 
-                // get the project name
-                const project = entry.project || 'No project';
+                // create the summary of the day
+                const summary = historyItems[day].map(entry => {
+                    // get the duration of the entry
+                    let duration = 'Running';
+                    if ('stop' in entry) {
+                        // sugarjs sets it auto on 1 hour, so i'll just subtract 3600000 for now, fix this later
+                        duration = Sugar.Date.format(Sugar.Date.create(entry.stop - entry.start - 3600000), '%H:%M:%S');
+                    }
 
-                // get the start date
-                const startDate = Sugar.Date.format(Sugar.Date.create(entry.start), '%c')
+                    // get the project name
+                    const project = entry.project || 'No project';
 
-                return {
+                    // when did we start?
+                    const startDate = Sugar.Date.format(Sugar.Date.create(entry.start), '%H:%M:%S');
+
+                    return `[${duration}] ${project} (start: ${startDate})`;
+                })
+
+                // const historyDate = Sugar.Date.create(dayStart).format('%F');
+                result.push({
                     icon: 'fa-clock-o',
-                    title: `[${duration}] ${project}`,
-                    subtitle: `started on ${startDate}`
-                }
+                    title: day,
+                    subtitle: `Select to copy the summary`,
+                    preview: `<div style="font-family: sans-serif; font-size: 12px;">${summary.join('<br>')}</div>`
+                });
+            }
 
-            }));
+            resolve(result);
         });
     }
 }
