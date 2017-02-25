f7d9d0a57f691a4d9104ab00c75e1cca8cb2142f
Jens Trio <hello@betaweb.be>
2017-02-25 12:05:31 +0100

    Use sugarjs for date parsing

--- src/history.js
+++ src/history.js
@@ -1,4 +1,5 @@
 const timetrack = require('./timetrack');
+const Sugar = require('sugar-date');
 const calculateDuration = require('./calculateDuration');
 
 module.exports = (pluginContext) => {
@@ -6,19 +7,27 @@ module.exports = (pluginContext) => {
         return new Promise((resolve, reject) => {
             timetrack.loadData();
             resolve(timetrack.getHistory().map(entry => {
+                try {
                 // get the duration of the entry
                 let duration = 'Running';
                 if ('stop' in entry) {
-                    duration = calculateDuration(entry.start, entry.stop);
+                    // sugarjs sets it auto on 1 hour, so i'll just subtract 3600000 for now, fix this later
+                    duration = Sugar.Date.format(Sugar.Date.create(entry.stop - entry.start - 3600000), '%H:%M:%S');
                 }
 
                 // get the project name
                 const project = entry.project || 'No project';
 
+                // get the start date
+                const startDate = Sugar.Date.format(Sugar.Date.create(entry.start), '%c')
+
                 return {
                     icon: 'fa-clock-o',
                     title: `[${duration}] ${project}`,
-                    subtitle: `Nice work bro!`
+                    subtitle: `started on ${startDate}`
+                }
+                } catch (e) {
+                    alert('fount: ' + e);
                 }
             }));
         });
