dba7e170662191a4c616d8f43f5c25c82227e44f
Jens Trio <hello@betaweb.be>
2017-03-01 20:53:09 +0100

    Remove debug code

--- src/registerStart.js
+++ src/registerStart.js
@@ -3,13 +3,9 @@ const timetrack = require('./timetrack');
 module.exports = (pluginContext) => {
     return (projectName, env = {}) => {
         return new Promise((resolve, reject) => {
-            try {
             timetrack.loadData();
             timetrack.registerStart(projectName);
             const result = timetrack.save();
-            } catch (e) {
-                alert('fout: ' + e);
-            }
             resolve(result);
         });
     }
